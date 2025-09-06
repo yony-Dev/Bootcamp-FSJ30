c25aebf595f7002e7b7b82052cf976a490cdad99
yony-Dev <benitezalexander661@gmail.com>
2025-09-05 18:39:30 -0600

    Tarea React terminada

--- REACT/firebase-project/src/App.jsx
+++ REACT/firebase-project/src/App.jsx
@@ -3,7 +3,7 @@ import { LoginComponent } from './views/login/LoginComponents'
 import { Products } from './views/products/Products'
 import { BrowserRouter as Router, Routes, Route} from 'react-router'
 function App() {
-
+  //Genstion de rutas
   return (
     <Router>
       <Routes>
